8a5003fd8fab82389e07fd6955df41c2a0f692b1
eptrubnikova <eptrubnikova@gmail.com>
2022-10-06 14:34:27 +0600

    задача 1

--- src/lesson7/main.java
+++ src/lesson7/main.java
@@ -2,6 +2,15 @@ package lesson7;
 
 public class main {
     public static void main (String[] args){
+        System.out.println("Залача 1");
+        int salary = 15000;
+        int total = 0;
+        int a = 1;
+        for (; total < 2_459_000; a++) {
+            total = total + salary;}
+            System.out.println("Месяц " + a + ", сумма накоплений равна " + total + " рублей");
+
+        System.out.println("Задача 2");
 
     }
 }
